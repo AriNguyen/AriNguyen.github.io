22f6f224fd8471d05cb3fc51569e7a87a3b84767
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-08-30 17:06:49 -0400

    add imgBox

--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -3,6 +3,9 @@ import { Row, Col } from "react-flexbox-grid";
 
 import InfoBox from "../../components/infoBox/info-box";
 import Title from "../../components/title/title";
+import ImageBox from "../../components/imageBox/image-box";
+
+import Photo1 from "../../assets/blogs/blog3/star.jpg"
 
 import './about.scss';
 
@@ -17,17 +20,17 @@ export default function About() {
         </p>
         <Row>
           <Col md={12} lg={4}>
-            {/* <ImageBox image={Person01} title="Ari Nguyen" subTitle="Web designer" /> */}
+            <ImageBox image={Photo1} title="Ari Nguyen" subTitle="Web designer" />
             {/* <InfoBox title='Little About Me' info='abcd' boxFooter='Read More' /> */}
           </Col>
           <Col md={12} lg={4}>
             {/* <ImageBox title='tile' subTitle='subTitle' /> */}
-            <InfoBox title='Little About Me' info='When I was little, I was amazed at how tech innovations 
+            <InfoBox title='' info='When I was little, I was amazed at how tech innovations 
             transform our lives and businesses. Thus, I chose to major in Computer Science because it allows 
             me to pursue both of my passions: producing creative work and solving social problems.' boxFooter='' />
           </Col>
           <Col md={12} lg={4}>
-            <InfoBox title='Little About Me' info='I’m driven to solve meaningful and challenging real-world problems using technology. 
+            <InfoBox title='' info='I’m driven to solve meaningful and challenging real-world problems using technology. 
             I enjoy building projects with my friends. 
             I’ve attended 14 hackathons and achieved 7 awards. My projects focus on solving social good, accessibility, and environmental issues.' boxFooter='' />
           </Col>
