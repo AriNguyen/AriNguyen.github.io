5c1a1d8678969d44145219700912f80127e081af
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-09-07 18:10:28 -0400

    move projects array to projects.json

--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -4,19 +4,10 @@ import Masonry from "react-masonry-css";
 
 import './projects.scss';
 
+import projects from "./projects.json";
 import Title from "../../components/title/title";
 import ProjectBox from '../../components/projectBox/projectBox';
 import ProjectModal from "../../components/projectBox/projectModal";
-
-import Arrow from "../../assets/projects/arrow.svg";
-import Preview1 from "../../assets/projects/project1/preview.jpg";
-import Preview2 from "../../assets/projects/project2/preview.jpg";
-import Preview3 from "../../assets/projects/project3/preview.jpg";
-import Preview4 from "../../assets/projects/project4/preview.jpg";
-import Preview5 from "../../assets/projects/project5/preview.jpg";
-import Preview6 from "../../assets/projects/project6/preview.png";
-import Preview7 from "../../assets/projects/project7/preview.jpg";
-import Preview8 from "../../assets/projects/project8/preview.jpg";
 
 type project = {
   id: string,
@@ -40,97 +31,6 @@ export default function Projects() {
     imgs: Array<string>
   }>({id: "", preview: "", title: "", description: "", tag: [""], path: "", imgs: [""]});
   
-  let projects = [
-    {
-      id: "1",
-      preview: Preview1, 
-      title: "Speak Your Mind",
-      description: "Eye Tracking and Speech Generating App - Help People with Paralysis Communicate",
-      tag: ["robotics", "ai/ml"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/speakYourMind.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "2",
-      preview: Preview2,
-      title: "Object Avoidance and Detection Device",
-      description: "Allows visually impaired people navigate around easily and find a particular object with ease.",
-      tag: ["robotics", "ai/ml"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/objectAvoidance.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "3",
-      preview: Preview3,
-      title: "Smart Wheelchair For People with Disability",
-      description: "Help severely paralyzed patients navigate indoor and outdoor environments",
-      tag: ["robotics"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/wheelChair.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "4",
-      preview: Preview4,
-      title: "SmartFridge App",
-      description: "Mobile App to Reduce Food Waste",
-      tag: ["webMobile"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/delisa.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "5",
-      preview: Preview5,
-      title: "DeepSign Game",
-      description: "Interactive fun Unity Game for learning American Sign Language",
-      tag: ["ai/ml"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/deepSign.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "6",
-      preview: Preview6,
-      title: "Todo List using Speech Recognition Web App",
-      description: "",
-      tag: ["webMobile"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/todoList.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "7",
-      preview: Preview7,
-      title: "AURA",
-      description: "Virtual Assistant helps navigate around, find objects, understand the world",
-      tag: ["ai/ml", "robotics"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/aura.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-    {
-      id: "8",
-      preview: Preview8,
-      title: "CodeGrindr - Hackathon Team Forming App",
-      description: "A solution for effective Hackathon team formation",
-      tag: ["webMobile"],
-      path: "https://raw.githubusercontent.com/AriNguyen/project_readme/master/codeGrindr.md",
-      imgs: [
-        "https://github.com/AriNguyen/aringuyen.github.io/blob/master/src/assets/projects/project1/preview.jpg?raw=true",
-      ]
-    },
-  ];
-
   const [filterResult, setFilterResult] = useState(projects);
   const [pickedFilter, setPickedFilter] = useState("all");
   const [isFilterMenuActive, setFilterMenuActive] = useState(false);
@@ -205,7 +105,7 @@ export default function Projects() {
           <Col xs={12} sm={12} md={4} lg={3}>
             <div className="projects__filter" onMouseEnter={() => setFilterMenuActive(true)} onMouseLeave={() => setFilterMenuActive(false)}>
               <p className="font12">{pickedFilterDropdown} FIRST</p>
-              <img src={Arrow} alt="arrow" />
+              <img src="/assets/projects/arrow.svg" alt="arrow" />
               {isFilterMenuActive && (<div className="portfolio__filter-menu shadow">
                 <p className="font12" onClick={() => setPickedFilterDropdown("NEWEST")}>
                   NEWEST
