92440e537bf510526fc67568c1e5b72ced55f374
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-09-07 17:16:53 -0400

    add subtitle2 and title2 to info-Box

--- src/components/infoBox/info-box.tsx
+++ src/components/infoBox/info-box.tsx
@@ -4,17 +4,23 @@ import "./info-box.scss";
 
 type infoBoxProps = {
   title: string;
+  subTitle: string;
+  title2: string;
+  subTitle2: string;
   info: string;
   boxFooter: string;
 }
 
-export default function infoBox({title, info, boxFooter}: infoBoxProps) {
+export default function infoBox({title, subTitle, title2, subTitle2, info, boxFooter}: infoBoxProps) {
   return (
     <div className="info__box flex-center">
       <div>
-        <h4 className="font20 weight800">{title}</h4>
-        <p className="font15 weight500">{info}</p>
-        <p className="font15 weight500">{boxFooter}</p>
+        {title && <h4 className="font24 weight800">{title}</h4>}
+        {subTitle && <h3 className="font15 weight500">{subTitle}</h3>}
+        {title2 && <h4 className="font24 weight800">{title2}</h4>}
+        {subTitle2 && <h3 className="font15 weight500">{subTitle2}</h3>}
+        {info && <p className="font15 weight500">{info}</p>}
+        {boxFooter && <p className="font15 weight500">{boxFooter}</p>}
       </div>
     </div>
   ) 
