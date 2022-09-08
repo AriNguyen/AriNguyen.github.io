61e0b8c0babfc2259f3e6479dd18e41bf689a67b
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-09-07 20:34:46 -0400

    rename info-box to infoBox

--- src/components/infoBox/infoBox.tsx
+++ src/components/infoBox/infoBox.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 
-import "./info-box.scss";
+import "./infoBox.scss";
 
 type infoBoxProps = {
   title: string;
   subTitle: string;
-  title2: string;
-  subTitle2: string;
   info: string;
   boxFooter: string;
 }
 
-export default function infoBox({title, subTitle, title2, subTitle2, info, boxFooter}: infoBoxProps) {
+export default function InfoBox({title, subTitle, info, boxFooter}: infoBoxProps) {
   return (
     <div className="info__box flex-center">
       <div>
         {title && <h4 className="font24 weight800">{title}</h4>}
         {subTitle && <h3 className="font15 weight500">{subTitle}</h3>}
-        {title2 && <h4 className="font24 weight800">{title2}</h4>}
-        {subTitle2 && <h3 className="font15 weight500">{subTitle2}</h3>}
         {info && <p className="font15 weight500">{info}</p>}
         {boxFooter && <p className="font15 weight500">{boxFooter}</p>}
       </div>
