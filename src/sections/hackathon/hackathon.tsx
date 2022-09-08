9a0338378280cdc3bfd6470f5f5bdb2ae905d609
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-09-07 20:38:40 -0400

    encapsulate code to HackathonCard

--- src/sections/hackathon/hackathon.tsx
+++ src/sections/hackathon/hackathon.tsx
@@ -1,15 +1,9 @@
-import { Row, Col } from "react-flexbox-grid";
 import Swiper from "react-id-swiper";
 import { FcNext, FcPrevious } from 'react-icons/fc';
 
 import Title from "../../components/title/title";
-import InfoBox from "../../components/infoBox/info-box";
-import ImageBox from "../../components/imageBox/image-box";
+import HackathonCard from "../../components/hackathonCard/hackathonCard";
 
-// import Photo1 from "../../assets/blogs/blog1/hackru.jpg"
-// import Photo2 from "../../assets/blogs/blog1/hackpsu.jpg"
-// import Photo3 from "../../assets/blogs/blog1/dragonhacks.jpg"
-// import Photo4 from "../../assets/projects/project4/preview.jpg"
 import hackathons from "./hackathons.json";
 import "./hackathon.scss";
 
@@ -36,23 +30,13 @@ export default function Hackathon() {
     renderNextButton: () => <FcNext className="swiper-button-next swiper-button-big"></FcNext>,
   };
   return (
-    <div id="hackathons" className="section-wrapper white-color-background">
+    <div id="hackathons" className="section-wrapper main-color-background">
       <div className="wrapper">
         <Title title="Hackathons." />
         <Swiper {...params}>
           {hackathons && hackathons.map((hackathon) => (
             <div>
-              {/* <BlogBox article={hackathon} onClick={() => showSuccessModal(hackathon)} /> */}
-              <Row>
-                <Col md={8} lg={6}>
-                  <ImageBox image={hackathon.image} title="" subTitle="" />
-                </Col>
-                <Col md={8} lg={6}>
-                  <InfoBox title={hackathon.title} subTitle={hackathon.subTitle}
-                    title2={hackathon.title2} subTitle2={hackathon.subTitle2}
-                    info={hackathon.info} boxFooter={hackathon.boxFooter} />
-                </Col>
-              </Row>
+              <HackathonCard hackathon={hackathon} />
             </div>
           ))}
         </Swiper>
